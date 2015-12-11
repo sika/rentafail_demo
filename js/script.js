cedcece8647b301d74dae7aa0a0596cb3db13c33
Simon Karlsson <simon.karlsson@electrolux.com>
2015-12-11 17:23:40 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,74 @@
+/// <reference path="js/angular.min.js" />
+
+//module with route dependence
+var myApp = angular.module('MyModule', ['ngRoute']);
+/*router declaration*/
+myApp.config(fRouteProvider);
+/*router function*/
+function fRouteProvider($routeProvider) {
+	$routeProvider.when('/', {
+		controller : 'simpleController',
+		templateUrl : 'partials/home.html'
+	}).when('/beafail', {
+		controller : 'simpleController',
+		templateUrl : 'partials/be_a_fail.html'
+	}).otherwise({
+		redirectTo : '/'
+	});
+}
+
+/*controller declaration*/
+myApp.controller('simpleController', fSimpleController);
+/*controller function*/
+function fSimpleController($scope, simpleFactory) { //simpleFactory returns factory object
+	var customers = simpleFactory.getCustomers(); //call  factory for customer objects
+	$scope.customers = customers;
+}
+
+/*factory declaration*/
+myApp.factory('simpleFactory', fSimpleFactory);
+/*Factory function*/
+function fSimpleFactory() {
+	/*empty object declaration*/
+	var factory = {};
+	var customers = [{
+			name : "Dai Walton",
+			city : "Rocky View"
+		}, {
+			name : "Irma Mcgee",
+			city : "Columbia"
+		}, {
+			name : "Cole Case",
+			city : "Montignoso"
+		}
+	];
+
+	factory.getCustomers = function () {
+		return customers;
+	};
+	return factory;
+}
+
+
+
+/*
+var customers = [{
+name : "Dai Walton",
+city : "Rocky View"
+}, {
+name : "Irma Mcgee",
+city : "Columbia"
+}, {
+name : "Cole Case",
+city : "Montignoso"
+}
+];
+ */
+//myApp.factory(&#39;simpleFactory&#39;, simpleFactoryF);
+//function simpleFactoryF (){
+//var }
+
+//factory
+
+//controller
+//routeProvider
